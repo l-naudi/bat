787d6a1ffb5db87e12418a3501e7fbca92a2c9bd
l-naudi <61364405+l-naudi@users.noreply.github.com>
2020-03-30 17:25:47 -0400

    Revert "Revert "woodcutting plugin cleanup / fixees""
    This reverts commit 16d30253f742f02f5aca72c20174726b795f559a.

--- game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/woodcutting/Woodcutting.kt
+++ game/plugins/src/main/kotlin/gg/rsmod/plugins/content/skills/woodcutting/Woodcutting.kt
@@ -8,24 +8,20 @@ import gg.rsmod.game.model.queue.QueueTask
 import gg.rsmod.plugins.api.Skills
 import gg.rsmod.plugins.api.ext.*
 
-/**
- * @author Tom <rspsmods@gmail.com>
- */
 object Woodcutting {
 
-    data class Tree(val type: TreeType, val obj: Int, val trunk: Int)
-
-    suspend fun chopDownTree(it: QueueTask, obj: GameObject, tree: TreeType, trunkId: Int) {
+    suspend fun chop(it: QueueTask, obj: GameObject, tree: Tree) {
         val p = it.player
 
         if (!canChop(p, obj, tree)) {
             return
         }
 
-        val logName = p.world.definitions.get(ItemDef::class.java, tree.log).name
-        val axe = AxeType.values.firstOrNull { p.getSkills().getMaxLevel(Skills.WOODCUTTING) >= it.level && (p.equipment.contains(it.item) || p.inventory.contains(it.item)) }!!
+        val log = p.world.definitions.get(ItemDef::class.java, tree.id).name.toLowerCase()
+        val axe = Axe.bestFor(p)!!
 
         p.filterableMessage("You swing your axe at the tree.")
+
         while (true) {
             p.animate(axe.animation)
             it.wait(2)
@@ -36,39 +32,48 @@ object Woodcutting {
             }
 
             val level = p.getSkills().getCurrentLevel(Skills.WOODCUTTING)
+
             if (level.interpolate(minChance = 60, maxChance = 190, minLvl = 1, maxLvl = 99, cap = 255)) {
-                p.filterableMessage("You get some ${logName.pluralSuffix(2)}.")
+                p.filterableMessage("You get some ${log.pluralSuffix(2)}.")
                 p.playSound(3600)
-                p.inventory.add(tree.log)
+
+                p.inventory.add(tree.id)
                 p.addXp(Skills.WOODCUTTING, tree.xp)
 
-                if (p.world.random(tree.depleteChance) == 0) {
+                if (p.world.random(tree.chance) == 0) {
                     p.animate(-1)
 
-                    if (trunkId != -1) {
-                        val world = p.world
-                        world.queue {
-                            val trunk = DynamicObject(obj, trunkId)
-                            world.remove(obj)
-                            world.spawn(trunk)
-                            wait(tree.respawnTime.random())
-                            world.remove(trunk)
-                            world.spawn(DynamicObject(obj))
-                        }
+                    val world = p.world
+
+                    world.queue {
+                        val stump = DynamicObject(obj, tree.stump)
+
+                        world.remove(obj)
+                        world.spawn(stump)
+
+                        wait(tree.respawn.random())
+
+                        world.remove(stump)
+                        world.spawn(DynamicObject(obj))
                     }
+
                     break
                 }
+
+                // TODO birds nests
             }
+
             it.wait(2)
         }
     }
 
-    private fun canChop(p: Player, obj: GameObject, tree: TreeType): Boolean {
+    private fun canChop(p: Player, obj: GameObject, tree: Tree): Boolean {
         if (!p.world.isSpawned(obj)) {
             return false
         }
 
-        val axe = AxeType.values.firstOrNull { p.getSkills().getMaxLevel(Skills.WOODCUTTING) >= it.level && (p.equipment.contains(it.item) || p.inventory.contains(it.item)) }
+        val axe = Axe.bestFor(p)
+
         if (axe == null) {
             p.message("You need an axe to chop down this tree.")
             p.message("You do not have an axe which you have the woodcutting level to use.")
